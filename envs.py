4152fe0012dc349c33a9de87974f5fb80cbc24ec
Lukas Palmer <lukas@lnpalmer.io>
2017-11-26 10:01:20 -0500

    don't clip rewards

--- envs.py
+++ envs.py
@@ -11,7 +11,7 @@ from multiprocessing import Process, Pipe
 def make_env(env_name, rank, seed):
     env = make_atari(env_name)
     env.seed(seed + rank)
-    env = wrap_deepmind(env, episode_life=False)
+    env = wrap_deepmind(env, episode_life=False, clip_rewards=False)
     return env
 
 
